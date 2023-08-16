f171bd6890bca8c772040b9e1501195413458be7
Juan Duarte <david.duarte@sisloga.com>
2023-08-15 22:45:57 -0500

    Decoradores

--- 01-typescript-intro/src/main.ts
+++ 01-typescript-intro/src/main.ts
@@ -4,8 +4,9 @@
 // import './topics/06-functions-destructuring';
 // import './topics/07-import-export'
 // import './topics/08-classes'
-import './topics/081-constructor'
-import './topics/09-genericos'
+// import './topics/081-constructor'
+// import './topics/09-genericos'
+import './topics/10-decorators'
 
 
 import './style.css'
