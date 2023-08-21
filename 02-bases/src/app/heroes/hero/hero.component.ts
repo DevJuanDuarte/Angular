5f79b42ff178b9687534a7c5110b3f10e373b34d
Juan Duarte <david.duarte@sisloga.com>
2023-08-21 16:29:05 -0500

    ngIf

--- 02-bases/src/app/heroes/hero/hero.component.ts
+++ 02-bases/src/app/heroes/hero/hero.component.ts
@@ -8,4 +8,26 @@ import { Component } from '@angular/core';
 export class HeroComponent {
   public name:string = 'ironman';
   public age: number = 45;
+
+
+  get capitalizedName():string {
+    return this.name.toUpperCase();
+  }
+
+  getHeroDescription():string {
+    return `${this.name} -  ${this.age}`;
+  }
+
+  changeName():void {
+    this.name = 'Spiderman';
+  }
+
+  changeAge():void {
+    this.age = 25;
+  }
+
+  reset():void{
+    this.name = 'ironman';
+    this.age = 45;
+  }
 }
