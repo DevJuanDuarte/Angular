b95151532903164461f4bb455e4eb3576c828676
David Duarte <david.duarte@sisloga.com>
2023-08-22 12:27:57 -0500

    Modulos

--- 02-bases/src/app/app.module.ts
+++ 02-bases/src/app/app.module.ts
@@ -1,20 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CounterModule } from './counter/counter.module';
 
 import { AppComponent } from './app.component';
-import { CounterComponent } from './counter/counter.component';
 import { HeroComponent } from './heroes/hero/hero.component';
 import { ListComponent } from './heroes/list/list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CounterComponent,
     HeroComponent,
     ListComponent  
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CounterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
