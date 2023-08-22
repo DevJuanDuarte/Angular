8dbfeaa95c778fd20598218da0b4432790176a7b
David Duarte <david.duarte@sisloga.com>
2023-08-21 20:05:13 -0500

    ngIf y ng-template

--- 02-bases/src/app/heroes/list/list.component.ts
+++ 02-bases/src/app/heroes/list/list.component.ts
@@ -8,4 +8,11 @@ import { Component } from '@angular/core';
 export class ListComponent {
 
   public heroNames: string[] = ['Spiderman', 'Ironman', 'Hulk', 'She Hulk', 'Thor'];
+  public deletedHero?:string;
+
+  removeLastHero():void{
+    this.deletedHero = this.heroNames.pop();
+  }
+
+
 }
