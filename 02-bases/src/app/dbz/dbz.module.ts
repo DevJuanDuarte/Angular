be57a948556bc86b5d6f5af4e87d25533b05c079
Juan Duarte <david.duarte@sisloga.com>
2023-08-26 17:57:32 -0500

    Componentes mas pequenos

--- 02-bases/src/app/dbz/dbz.module.ts
+++ 02-bases/src/app/dbz/dbz.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DbzComponent } from './pages/main-page.component';
+import { ListComponent } from './components/list/list.component';
+import { AddCharacter } from './components/add/add.component';
 
 
 
 @NgModule({
   declarations: [
-    DbzComponent
+    DbzComponent,
+    ListComponent,
+    AddCharacter
   ],
   imports: [
     CommonModule
