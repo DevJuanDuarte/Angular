7fd9f677b1e829c95d8b34eb0dab6a56a20057ed
Juan Duarte <david.duarte@sisloga.com>
2023-08-29 00:32:24 -0500

    Forms Module y ngModule

--- 02-bases/src/app/dbz/components/add/add.component.ts
+++ 02-bases/src/app/dbz/components/add/add.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
   selector: 'dbz-add-character',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AddCharacterComponent {
 
+  public character:Character = {
+    name:'',
+    power: 0
+  }
+
+  emitCharacter():void {
+    console.log(this.character);
+  } 
 }
