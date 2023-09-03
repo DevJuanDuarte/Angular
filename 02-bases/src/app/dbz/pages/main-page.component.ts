691adcc14877a35fc34e24b0a85e1aec0e18d15e
Juan Duarte <david.duarte@sisloga.com>
2023-09-03 17:09:46 -0500

    @onSubmit y @Output

--- 02-bases/src/app/dbz/pages/main-page.component.ts
+++ 02-bases/src/app/dbz/pages/main-page.component.ts
@@ -2,19 +2,30 @@ import { Component } from '@angular/core';
 import { Character } from '../interfaces/character.interface';
 
 @Component({
-    selector: 'app-dbz-main-page',
-    templateUrl: './main-page.component.html'
+  selector: 'app-dbz-main-page',
+  templateUrl: './main-page.component.html',
 })
-
 export class MainPageComponent {
-    public characters: Character[] = [{
-        name: 'Krillin',
-        power: 1000
-    },{
-        name:'Goku',
-        power: 9999
-    },{
-        name: 'Vegeta',
-        power: 7777
-    }];
+  public characters: Character[] = [
+    {
+      name: 'Krillin',
+      power: 1000,
+    },
+    {
+      name: 'Goku',
+      power: 9999,
+    },
+    {
+      name: 'Vegeta',
+      power: 7777,
+    },
+  ];
+
+  onNewCharacter(character:Character): void {
+    this.characters.push(character);
+  }
+
+  onDeleteCharacter(index: number):void {
+    this.characters.splice(index, 1)
+  }
 }
