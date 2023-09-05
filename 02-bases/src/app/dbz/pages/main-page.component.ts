696a8c50bb94adaadd2fccbb18fa01ae41d1c91d
Juan Duarte <david.duarte@sisloga.com>
2023-09-04 22:48:08 -0500

    Services

--- 02-bases/src/app/dbz/pages/main-page.component.ts
+++ 02-bases/src/app/dbz/pages/main-page.component.ts
@@ -1,31 +1,11 @@
 import { Component } from '@angular/core';
 import { Character } from '../interfaces/character.interface';
+import { DbzService } from '../services/dbz.service';
 
 @Component({
   selector: 'app-dbz-main-page',
   templateUrl: './main-page.component.html',
 })
 export class MainPageComponent {
-  public characters: Character[] = [
-    {
-      name: 'Krillin',
-      power: 1000,
-    },
-    {
-      name: 'Goku',
-      power: 9999,
-    },
-    {
-      name: 'Vegeta',
-      power: 7777,
-    },
-  ];
-
-  onNewCharacter(character:Character): void {
-    this.characters.push(character);
-  }
-
-  onDeleteCharacter(index: number):void {
-    this.characters.splice(index, 1)
-  }
+  constructor(public dbzService: DbzService) {}
 }
