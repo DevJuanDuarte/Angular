e4b61fa65ceaea7af3e1fb71e8f41f50f072765d
Juan Duarte <david.duarte@sisloga.com>
2023-09-08 01:54:24 -0500

    uuid

--- 02-bases/src/app/dbz/services/dbz.service.ts
+++ 02-bases/src/app/dbz/services/dbz.service.ts
@@ -1,29 +1,43 @@
 import { Injectable } from '@angular/core';
 import { Character } from '../interfaces/character.interface';
+import { v4 as uuid } from "uuid";
+
+// console.log(uuid());
 
 @Injectable({providedIn: 'root'})
 export class DbzService {
     
     public characters: Character[] = [
         {
+          id:uuid(),
           name: 'Krillin',
           power: 1000,
         },
         {
+          id:uuid(),
           name: 'Goku',
           power: 9999,
         },
         {
+          id:uuid(),
           name: 'Vegeta',
           power: 7777,
         },
       ];
     
       onNewCharacter(character:Character): void {
-        this.characters.push(character);
+
+        const newCharacter: Character = {id: uuid(), ...character};
+
+        this.characters.push(newCharacter);
       }
     
-      onDeleteCharacter(index: number):void {
-        this.characters.splice(index, 1)
+      // onDeleteCharacter(index: number):void {
+      //   this.characters.splice(index, 1)
+      // }
+
+      deleteCharacterById (id: string) {
+        this.characters = this.characters.filter(character => character.id != id);
+
       }
 }
