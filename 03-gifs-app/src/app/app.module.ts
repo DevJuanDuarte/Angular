76d1e4a2353b2648f202a295335937666422d3eb
Juan Duarte <david.duarte@sisloga.com>
2023-09-23 00:12:24 -0500

    gifsapp

---
+++ 03-gifs-app/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { GifsModule } from './gifs/gifs.module';
+import { SharedModule } from './shared/shared.module';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    GifsModule,
+    SharedModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
