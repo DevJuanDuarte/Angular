7313830842c8d689d92e511926c40b73e7d37aac
Juan Duarte <david.duarte@sisloga.com>
2023-09-24 18:17:30 -0500

    avances componente de homepage

--- 03-gifs-app/src/app/gifs/gifs.module.ts
+++ 03-gifs-app/src/app/gifs/gifs.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomePageComponent } from './pages/home/home-page.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomePageComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports
+  : [
+    HomePageComponent
   ]
 })
 export class GifsModule { }
