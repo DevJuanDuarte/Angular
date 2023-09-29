f7212a719a374a95c09809773e2dd1e4b82cb594
Juan Duarte <david.duarte@sisloga.com>
2023-09-29 00:21:17 -0500

    card list

--- 03-gifs-app/src/app/gifs/components/card-list/card-list.component.ts
+++ 03-gifs-app/src/app/gifs/components/card-list/card-list.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'card-list',
+  selector: 'gifs-card-list',
   templateUrl: './card-list.component.html'
 })
 export class CardListComponent {
