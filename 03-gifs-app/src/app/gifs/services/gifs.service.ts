a916f488c2088beb69e02654ced08d3e6d5a4e92
David Duarte <david.duarte@sisloga.com>
2023-10-17 00:02:16 -0500

    Local storage

--- 03-gifs-app/src/app/gifs/services/gifs.service.ts
+++ 03-gifs-app/src/app/gifs/services/gifs.service.ts
@@ -11,7 +11,9 @@ export class GifsService {
     private apiKey:       string = "7OmCR5CSjA1PdiSiGmk5ng9a9Fv8JlVl";
     private serviceUrl:   string = 'https://api.giphy.com/v1/gifs';
 
-    constructor( private http: HttpClient ) { }
+    constructor( private http: HttpClient ) { 
+        this.loadLocalStorage();
+     }
 
     get tagsHistory () {
         return [...this._tagsHistory];
@@ -26,6 +28,20 @@ export class GifsService {
 
         this._tagsHistory.unshift(tag);
         this._tagsHistory = this.tagsHistory.splice(0,10);
+        this.saveLocalStorage();
+    }
+
+    private saveLocalStorage(): void {
+        localStorage.setItem('history', JSON.stringify( this._tagsHistory ));
+    }
+
+    private loadLocalStorage():void{
+        if( !localStorage.getItem('history') ) return;
+        this._tagsHistory = JSON.parse( localStorage.getItem('history')!);
+
+        if( this._tagsHistory.length === 0) return;
+        this.searchTag( this._tagsHistory[0]);
+
     }
 
     searchTag( tag : string ):void {
