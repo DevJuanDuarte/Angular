3ff48cfd4983938fb03cd55a70b099fea4ad6f71
Juan Duarte <david.duarte@sisloga.com>
2023-10-29 13:54:17 -0500

    Creacion de proyecto, RouterModule y Paginas SPA

---
+++ countryApp/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    AppRoutingModule,
+    BrowserModule,
+    SharedModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ countryApp/src/app/shared/shared.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+
+
+
+@NgModule({
+  declarations: [
+    AboutPageComponent,
+    HomePageComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    AboutPageComponent,
+    HomePageComponent
+  ]
+})
+export class SharedModule { }
