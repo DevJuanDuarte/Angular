6ae253030826161c938f0e64e6227d7715e22b8e
David Duarte <david.duarte@sisloga.com>
2023-10-31 18:07:18 -0500

    RouterLink

--- countryApp/src/app/shared/shared.module.ts
+++ countryApp/src/app/shared/shared.module.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
@@ -6,19 +5,12 @@ import { AboutPageComponent } from './pages/about-page/about-page.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { SidebarComponent } from './pages/sidebar/sidebar.component';
 import { ContactPageComponent } from './pages/contact-page/contact-page.component';
-=======
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { AboutPageComponent } from './pages/about-page/about-page.component';
-import { HomePageComponent } from './pages/home-page/home-page.component';
->>>>>>> 3ff48cfd4983938fb03cd55a70b099fea4ad6f71
 
 
 
 @NgModule({
   declarations: [
     AboutPageComponent,
-<<<<<<< HEAD
     HomePageComponent,
     SidebarComponent,
     ContactPageComponent,
@@ -32,16 +24,6 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
     HomePageComponent,
     SidebarComponent,
     ContactPageComponent
-=======
-    HomePageComponent
-  ],
-  imports: [
-    CommonModule
-  ],
-  exports: [
-    AboutPageComponent,
-    HomePageComponent
->>>>>>> 3ff48cfd4983938fb03cd55a70b099fea4ad6f71
   ]
 })
 export class SharedModule { }
