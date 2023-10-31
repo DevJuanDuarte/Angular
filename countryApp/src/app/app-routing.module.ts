c0ece28a0e5c14ad99451c2878fa8d15791b6a87
David Duarte <david.duarte@sisloga.com>
2023-10-31 18:15:30 -0500

    Ajustes

--- countryApp/src/app/app-routing.module.ts
+++ countryApp/src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './shared/pages/home-page/home-page.component';
 import { AboutPageComponent } from './shared/pages/about-page/about-page.component';
-<<<<<<< HEAD
 import { ContactPageComponent } from './shared/pages/contact-page/contact-page.component';
 
 const routes: Routes = [
@@ -22,29 +21,11 @@ const routes: Routes = [
         path: '**',
         redirectTo: ''
     }
-=======
-
-const routes: Routes  =[
-    {
-        path: 'home',
-        component: HomePageComponent
-    },{
-        path: 'about',
-        component: AboutPageComponent
-    },{
-        path: '**',
-        redirectTo: 'home'
-    },
->>>>>>> 3ff48cfd4983938fb03cd55a70b099fea4ad6f71
 ]
 
 @NgModule({
     imports: [
-<<<<<<< HEAD
         RouterModule.forRoot( routes ),
-=======
-        RouterModule.forRoot ( routes ),
->>>>>>> 3ff48cfd4983938fb03cd55a70b099fea4ad6f71
     ],
     exports: [
         RouterModule
