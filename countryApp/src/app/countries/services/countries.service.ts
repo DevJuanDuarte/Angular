a415b675fab320490e816365f783f49df6a4284a
David Duarte <david.duarte@sisloga.com>
2023-11-06 22:49:39 -0500

    Argumentos por url, switchMap, Mostrar Info del pais, Navegar hacia pagina de pais

--- countryApp/src/app/countries/services/countries.service.ts
+++ countryApp/src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, catchError, of } from 'rxjs';
+import { Observable, catchError, map, of } from 'rxjs';
 import { Country } from '../interfaces/country';
 
 @Injectable({providedIn: 'root'})
@@ -9,6 +9,18 @@ export class CountriesService {
     private apiUrl: string = 'https://restcountries.com/v3.1';
 
     constructor(private http: HttpClient){  }
+
+    searchCountryByAplhaCode(code:string): Observable<Country | null>{
+        return this.http.get<Country[]>(`${this.apiUrl}/alpha/${ code }`)
+        .pipe(
+            map(countries => countries.length > 0 ? countries[0]: null),
+            //Manejo de errores en observables: en caso de buscar un registro que no existe devuelve un arreglo vacio
+            catchError( error => {
+                console.log(error);
+                return of(null)
+            })
+        )
+    }
 
     searchCapital (term: string): Observable<Country[]>  {
         return this.http.get<Country[]>(`${this.apiUrl}/capital/${ term }`)
