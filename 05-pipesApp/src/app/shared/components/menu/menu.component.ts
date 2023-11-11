7d33eddf0f12926b1ddd55d9428be5ed1cbe4ff5
Juan Duarte <david.duarte@sisloga.com>
2023-11-11 11:35:23 -0500

    Instalacion primeicons, ripple con PrimeNGModule, Creacion del Menu

---
+++ 05-pipesApp/src/app/shared/components/menu/menu.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+  selector: 'shared-menu',
+  templateUrl: './menu.component.html',
+  styles: [
+  ]
+})
+export class MenuComponent {
+  public menuItems: MenuItem[] = [];
+
+    ngOnInit() {
+        this.menuItems = [
+            {
+                label: 'New',
+                icon: 'pi pi-fw pi-plus',
+            },
+            {
+                label: 'Delete',
+                icon: 'pi pi-fw pi-trash'
+            }
+        ];
+    }
+}
