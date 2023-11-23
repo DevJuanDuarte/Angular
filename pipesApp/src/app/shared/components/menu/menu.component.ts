615dd5158ee972e7b8023233547b597dbf3eecb8
Juan Duarte <david.duarte@sisloga.com>
2023-11-23 17:12:37 -0500

    Navegando con el MenuBar

--- pipesApp/src/app/shared/components/menu/menu.component.ts
+++ pipesApp/src/app/shared/components/menu/menu.component.ts
@@ -13,60 +13,28 @@ export class MenuComponent {
   ngOnInit() {
     this.menuItems = [
         {
-            label: 'File',
-            icon: 'pi pi-fw pi-file',
+            label: 'Pipes de Angular',
+            icon: 'pi pi-check-square',
             items: [
                 {
-                    label: 'New',
-                    icon: 'pi pi-fw pi-plus',
-                    items: [
-                        {
-                            label: 'Bookmark',
-                            icon: 'pi pi-fw pi-bookmark'
-                        },
-                        {
-                            label: 'Video',
-                            icon: 'pi pi-fw pi-video'
-                        }
-                    ]
+                    label: 'Textos y Fechas',
+                    icon: 'pi pi-file',
+                    routerLink:'/'
                 },
                 {
-                    label: 'Delete',
-                    icon: 'pi pi-fw pi-trash'
+                    label: 'Numeros',
+                    icon: 'pi pi-hashtag',
+                    routerLink:'/numbers'
                 },
                 {
-                    separator: true
-                },
-                {
-                    label: 'Export',
-                    icon: 'pi pi-fw pi-external-link'
+                    label: 'Incomunes',
+                    icon: 'pi pi-slack',
+                    routerLink:'/uncommon'
                 }
             ]
         },
         {
-            label: 'Edit',
-            icon: 'pi pi-fw pi-pencil',
-            items: [
-                {
-                    label: 'Left',
-                    icon: 'pi pi-fw pi-align-left'
-                },
-                {
-                    label: 'Right',
-                    icon: 'pi pi-fw pi-align-right'
-                },
-                {
-                    label: 'Center',
-                    icon: 'pi pi-fw pi-align-center'
-                },
-                {
-                    label: 'Justify',
-                    icon: 'pi pi-fw pi-align-justify'
-                }
-            ]
-        },
-        {
-            label: 'Users',
+            label: 'Pipes Personalizados',
             icon: 'pi pi-fw pi-user',
             items: [
                 {
@@ -98,40 +66,6 @@ export class MenuComponent {
                     ]
                 }
             ]
-        },
-        {
-            label: 'Events',
-            icon: 'pi pi-fw pi-calendar',
-            items: [
-                {
-                    label: 'Edit',
-                    icon: 'pi pi-fw pi-pencil',
-                    items: [
-                        {
-                            label: 'Save',
-                            icon: 'pi pi-fw pi-calendar-plus'
-                        },
-                        {
-                            label: 'Delete',
-                            icon: 'pi pi-fw pi-calendar-minus'
-                        }
-                    ]
-                },
-                {
-                    label: 'Archieve',
-                    icon: 'pi pi-fw pi-calendar-times',
-                    items: [
-                        {
-                            label: 'Remove',
-                            icon: 'pi pi-fw pi-calendar-minus'
-                        }
-                    ]
-                }
-            ]
-        },
-        {
-            label: 'Quit',
-            icon: 'pi pi-fw pi-power-off'
         }
     ];
 }
