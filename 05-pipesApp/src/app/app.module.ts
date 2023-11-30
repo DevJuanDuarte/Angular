84c70a783ade7b030b325aeb69ea7978cf2e4333
Juan Duarte <david.duarte@sisloga.com>
2023-11-30 15:04:13 -0500

    Pipes avances

--- 05-pipesApp/src/app/app.module.ts
+++ 05-pipesApp/src/app/app.module.ts
@@ -1,10 +1,15 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ButtonModule } from 'primeng/button';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
+
+//Configuracion de locale de la app:
+import localeEsCO from "@angular/common/locales/es-CO";
+import { registerLocaleData } from "@angular/common";
+
+registerLocaleData(localeEsCO);
 
 @NgModule({
   declarations: [
@@ -13,11 +18,12 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule,
     BrowserAnimationsModule,
-    ButtonModule
+    SharedModule
   ],
-  providers: [],
+  providers: [
+    {provide:LOCALE_ID, useValue:'es-CO'}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- 05-pipesApp/src/app/shared/components/menu/menu.component.ts
+++ 05-pipesApp/src/app/shared/components/menu/menu.component.ts
@@ -10,16 +10,63 @@ import { MenuItem } from 'primeng/api';
 export class MenuComponent {
   public menuItems: MenuItem[] = [];
 
-    ngOnInit() {
-        this.menuItems = [
-            {
-                label: 'New',
-                icon: 'pi pi-fw pi-plus',
-            },
-            {
-                label: 'Delete',
-                icon: 'pi pi-fw pi-trash'
-            }
-        ];
-    }
+  ngOnInit() {
+    this.menuItems = [
+        {
+            label: 'Pipes de Angular',
+            icon: 'pi pi-check-square',
+            items: [
+                {
+                    label: 'Textos y Fechas',
+                    icon: 'pi pi-file',
+                    routerLink:'/'
+                },
+                {
+                    label: 'Numeros',
+                    icon: 'pi pi-hashtag',
+                    routerLink:'/numbers'
+                },
+                {
+                    label: 'Incomunes',
+                    icon: 'pi pi-slack',
+                    routerLink:'/uncommon'
+                }
+            ]
+        },
+        {
+            label: 'Pipes Personalizados',
+            icon: 'pi pi-fw pi-user',
+            items: [
+                {
+                    label: 'New',
+                    icon: 'pi pi-fw pi-user-plus'
+                },
+                {
+                    label: 'Delete',
+                    icon: 'pi pi-fw pi-user-minus'
+                },
+                {
+                    label: 'Search',
+                    icon: 'pi pi-fw pi-users',
+                    items: [
+                        {
+                            label: 'Filter',
+                            icon: 'pi pi-fw pi-filter',
+                            items: [
+                                {
+                                    label: 'Print',
+                                    icon: 'pi pi-fw pi-print'
+                                }
+                            ]
+                        },
+                        {
+                            icon: 'pi pi-fw pi-bars',
+                            label: 'List'
+                        }
+                    ]
+                }
+            ]
+        }
+    ];
+}
 }
