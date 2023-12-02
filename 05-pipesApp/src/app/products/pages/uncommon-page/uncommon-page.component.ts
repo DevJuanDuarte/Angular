6726f81a6917e81ff158c0140e6aa20e24a122a0
Juan Duarte <david.duarte@sisloga.com>
2023-12-02 10:44:10 -0500

    i18nSelect

--- 05-pipesApp/src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ 05-pipesApp/src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-uncommon-page',
   templateUrl: './uncommon-page.component.html',
-  styleUrls: ['./uncommon-page.component.css']
+  styleUrls: ['./uncommon-page.component.css'],
 })
 export class UncommonPageComponent {
+  //i18nSelect:
+  public name: string = 'Juan';
+  public gender: 'male' | 'female' = 'male';
+  public invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla',
+  };
 
+  changeCliente(): void {
+    this.name = 'Melissa';
+    this.gender = 'female';
+  }
 }
