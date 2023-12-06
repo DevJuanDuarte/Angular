95053b1e5e8e9c6fbb7de5b46476d2dde5062f57
Juan Duarte <david.duarte@sisloga.com>
2023-12-06 17:15:38 -0500

    Rutas padres e hijas

--- 06-heroesApp/src/app/app-routing.module.ts
+++ 06-heroesApp/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Error404PageComponent } from './shared/pages/error404-page/error404-page.component';
 
+//Primeros descendientes.
 const routes: Routes = [
   {
     path: 'auth',
