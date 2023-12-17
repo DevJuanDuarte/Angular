acd0717053795d733b4ae8ddc74934ad7980bb1e
Juan Duarte <david.duarte@sisloga.com>
2023-12-17 17:24:44 -0500

    Config del dasboard con tailwind, @for (Nueva sintaxis)

--- my-dashboard/src/app/shared/sidemenu/sidemenu.component.ts
+++ my-dashboard/src/app/shared/sidemenu/sidemenu.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { routes } from '../../app.routes';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sidemenu',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './sidemenu.component.html',
   styleUrl: './sidemenu.component.css'
 })
 export class SidemenuComponent {
+  public menuItems = routes
+    .map(route => route.children ?? [])
+    .flat()
+    .filter(route => route && route.path)
+    .filter( route => !route.path?.includes(':'))
+  constructor() {
+    // const dashboardRoutes = routes
+    // .map(route => route.children ?? [])
+    // .flat()
+    // .filter(route => route && route.path)
+    // .filter( route => !route.path?.includes(':'))
+    // console.log(dashboardRoutes);
 
+  }
 }
