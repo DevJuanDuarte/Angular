4048571d19cf5c28cf88f49344e7c9176825b59d
Juan Duarte <david.duarte@sisloga.com>
2024-01-12 00:03:10 -0500

    Nueva sintaxis

--- my-dashboard/src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ my-dashboard/src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
 
+type Grade = 'A' | 'B' | 'F'
 @Component({
   selector: 'app-control-flow',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './control-flow.component.html',
-  styles: ``
+  styles: ``,
 })
 export default class ControlFlowComponent {
+  //Creamos una variable que sera una señal
+  public showcontent = signal(false);
+  public grade = signal<Grade>('B');
 
+  //Creamos un metodo donde llamamos a la señal y le damos una propiedad
+  //De actualizacion de la señal llamando al valor y luego a la negacion de ese valor
+  public toogleContent() {
+    this.showcontent.update((value) => !value);
+  }
 }
