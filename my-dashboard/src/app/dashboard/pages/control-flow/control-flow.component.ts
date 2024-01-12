00bd1420d4770c46935df9bad404d4bbcade53d2
Juan Duarte <juan.devcontact@gmail.com>
2024-01-12 17:53:10 -0500

    @for

--- my-dashboard/src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ my-dashboard/src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 
-type Grade = 'A' | 'B' | 'F'
+type Grade = 'A' | 'B' | 'F';
 @Component({
   selector: 'app-control-flow',
   standalone: true,
@@ -13,6 +13,8 @@ export default class ControlFlowComponent {
   //Creamos una variable que sera una señal
   public showcontent = signal(false);
   public grade = signal<Grade>('B');
+  public frameworks = signal(['Angular', 'View', 'Svelte', 'React', 'OneMore']);
+  public frameworks2 = signal([]);
 
   //Creamos un metodo donde llamamos a la señal y le damos una propiedad
   //De actualizacion de la señal llamando al valor y luego a la negacion de ese valor
