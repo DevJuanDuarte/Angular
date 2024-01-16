c904b2bb94572ed5313d5501eda56beec219e007
Juan Duarte <juan.devcontact@gmail.com>
2024-01-16 18:00:40 -0500

    Defers

--- my-dashboard/src/app/dashboard/pages/defer-options/defer-options.component.ts
+++ my-dashboard/src/app/dashboard/pages/defer-options/defer-options.component.ts
@@ -1,12 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { HeavyLoaderFastComponent } from '@shared/heavy-loaders/heavy-loaders-fast.component';
+import { TitleComponent } from '@shared/title/title.component';
 
 @Component({
   selector: 'app-defer-options',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, TitleComponent, HeavyLoaderFastComponent],
   templateUrl: './defer-options.component.html',
-  styles: ``
+  styles: ``,
 })
-export default class DeferOptionsComponent {
-
-}
+export default class DeferOptionsComponent {}
--- my-dashboard/src/app/shared/heavy-loaders/heavy-loaders-slow.component.ts
+++ my-dashboard/src/app/shared/heavy-loaders/heavy-loaders-slow.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-heavy-loader-slow',
   standalone: true,
-  imports: [],
-  template: `<h1>Hola</h1>`,
-  styles: ``
+  imports: [CommonModule],
+  template: `<section [ngClass]="['w-full h-[600px]', cssClass]">
+    Heavy Loader Slow
+  </section>`
 })
 export class HeavyLoadersSlowComponent {
+  @Input({ required: true }) cssClass!: string;
 
+  constructor() {
+    const start = Date.now();
+    while (Date.now() - start < 3000) {}
+  }
 }
