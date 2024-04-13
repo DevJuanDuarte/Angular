bf5ccdbd80eb68113359728e372d39f14c529b18
Juan Duarte <juan.devcontact@gmail.com>
2024-04-12 23:54:57 -0500

    Agregando rutas

--- heroApp/src/app/heroes/heroes-routing.module.ts
+++ heroApp/src/app/heroes/heroes-routing.module.ts
@@ -1,7 +1,44 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { NewPageComponent } from './pages/new-page/new-page.component';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { ListPageComponent } from './pages/list-page/list-page.component';
+import { HeroPageComponent } from './pages/hero-page/hero-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutPageComponent,
+    children: [
+      {
+        path: 'new-hero',
+        component: NewPageComponent
+      },
+      {
+        path: 'search',
+        component: SearchPageComponent
+      },
+      {
+        path: 'edit/:id',
+        component: NewPageComponent
+      },
+      {
+        path: 'list',
+        component: ListPageComponent
+      },
+      //Si esta ruta se coloca al inicio todas las demas siempre van a pasar por aca
+      {
+        path: ':id',
+        component: HeroPageComponent
+      },
+      {
+        path:'**',
+        redirectTo: 'list'
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
