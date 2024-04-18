7f560d9a0de394ff86c566a0b1facc4a802bb148
Juan Duarte <juan.devcontact@gmail.com>
2024-04-18 15:05:41 -0500

    Obtener heroe por el URL

--- heroApp/src/app/heroes/pages/hero-page/hero-page.component.ts
+++ heroApp/src/app/heroes/pages/hero-page/hero-page.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeroService } from '../../services/heroes.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
+import { Hero } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-hero-page',
   templateUrl: './hero-page.component.html',
   styles: ``
 })
-export class HeroPageComponent {
+export class HeroPageComponent implements OnInit {
 
+  public hero?: Hero;
+
+  constructor(
+    //heroesService: Para llamar el servicio
+    private heroesService: HeroService,
+    private activateRoute: ActivatedRoute,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    //Leer cual es el URL utilizando el servicio activateRoute
+    this.activateRoute.params
+      .pipe(switchMap(({ id }) => this.heroesService.getHeroById(id))
+      )
+      .subscribe(hero => {
+        //Si el servicio devuelve un heroe que no existe entonces redirijo la persona a la lista de los heroes
+        if (!hero) return this.router.navigate(['/heroes/list']);
+
+        //Si existe establezco el heroe par mostrarlo en pantalla
+        this.hero = hero;
+        console.log({hero});
+        return;
+      })
+  }
 }
--- heroApp/src/app/heroes/services/heroes.service.ts
+++ heroApp/src/app/heroes/services/heroes.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { environments } from '../../../../environments/environments';
-import { Observable } from 'rxjs';
+import { catchError, Observable, of } from 'rxjs';
+
 import { Hero } from '../interfaces/hero.interface';
+import { environments } from '../../../../environments/environments';
 
 @Injectable({ providedIn: 'root' })
 export class HeroService {
@@ -11,7 +12,24 @@ export class HeroService {
 
   constructor(private httpClient: HttpClient) { }
 
-  getHeroes (): Observable<Hero[]> {
+  getHeroes(): Observable<Hero[]> {
     return this.httpClient.get<Hero[]>(`${this.baseUrl}/heroes`)
+  }
+
+  /*
+  TODO:Funcion para listar heroes por el id🤓:
+  1. Se crea el metodo llamado getHeroById
+  2. Se le pasa como parametro el id del heroe
+  3. Le funcion es un observable que puede ser de tipo Hero o undefined
+  4. Retorna el parametro httpClient para que llame al metodo get del service que tendra el Hero y dentro de los parentesis
+  se le pasa la ruta especifica de los heroes con el id
+  5. Se pasa un pipe luego de llamar la ruta para el manejo de errores.
+  */
+  
+  getHeroById(id: string): Observable<Hero | undefined> {
+    return this.httpClient.get<Hero>(`${this.baseUrl}/heroes/${id}`)
+      .pipe(
+        catchError(error => of(undefined))
+      )
   }
 }
